f268e6b87fa33ce4e0982bc9b0dec7ab51f25e9b
Omar Flores Salazar <omar.flores.salazar@uabc.edu.mx>
2019-08-28 11:14:45 -0700

    Se agrego la opcion de que si no hay datos almacenados en el ArrayList, se presentara un mensaje que no hay nada si es que el usuario pider mostrar informacion y no ha agregado nada anteriormente.

--- Algoritmos y Estructura de Datos/Laboratorio/Practica_1/Main.java
+++ Algoritmos y Estructura de Datos/Laboratorio/Practica_1/Main.java
@@ -15,6 +15,7 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
         Data data = new Data();
+        boolean avaible = false;
         Scanner teclado = new Scanner(System.in);
 
         // Ciclo indefinido hasta que el usuario desee salir.
@@ -28,41 +29,57 @@ public class Main {
                     // Crea el pais
                 case 1:
                     data.add(Interfaz.crearPais(teclado));
+                    avaible = true;
                     break;
                 // Muestra la informacion de inicio a fin
                 case 2:
-                    for (int i = 0; i < data.getSize(); i++)
-                        System.out.println("Pais No." + (i + 1) + data.mostrarPais(i));
+                    if (avaible)
+                        for (int i = 0; i < data.getSize(); i++)
+                            System.out.println("Pais No." + (i + 1) + data.mostrarPais(i));
+                    else
+                        System.out.println("No existen datos almacenados.");
                     break;
                 // Muestra la informacion de fin a inicio
                 case 3:
-                    for (int i = data.getSize() - 1; i >= 0; i--)
-                        System.out.println("Pais No." + (i + 1) + data.mostrarPais(i));
+                    if (avaible)
+                        for (int i = data.getSize() - 1; i >= 0; i--)
+                            System.out.println("Pais No." + (i + 1) + data.mostrarPais(i));
+                    else
+                        System.out.println("No existen datos almacenados.");
                     break;
                 // Muestra la informacion de la mitad al final
                 case 4:
-                    for (int i = data.getSize() / 2; i < data.getSize(); i++)
-                        System.out.println("Pais No." + (i + 1) + data.mostrarPais(i));
+                    if (avaible)
+                        for (int i = data.getSize() / 2; i < data.getSize(); i++)
+                            System.out.println("Pais No." + (i + 1) + data.mostrarPais(i));
+                    else
+                        System.out.println("No existen datos almacenados.");
                     break;
                 // Muestra la informacion de la mitad al principio
                 case 5:
-                    for (int i = data.getSize() / 2; i >= 0; i--)
-                        System.out.println("Pais No." + (i + 1) + data.mostrarPais(i));
+                    if (avaible)
+                        for (int i = data.getSize() / 2; i >= 0; i--)
+                            System.out.println("Pais No." + (i + 1) + data.mostrarPais(i));
+                    else
+                        System.out.println("No existen datos almacenados.");
                     break;
                 // Muestra la informacion de la ciudad basado en una busqueda de texto
                 case 6:
-                    System.out.println("Ingrese el nombre de la ciudad que desea buscar:");
-                    String name = teclado.nextLine();
-                    name = teclado.nextLine();
+                    if (avaible) {
+                        System.out.println("Ingrese el nombre de la ciudad que desea buscar:");
+                        String name = teclado.nextLine();
+                        name = teclado.nextLine();
 
-                    for (int i = 0; i < data.getSize(); i++) {
-                        if (data.getData().get(i).getCity().toLowerCase().equals(name.toLowerCase())) {
-                            System.out.println("Pais No." + (i + 1) + data.mostrarPais(i));
-                            break;
-                        } else if (i == data.getSize() - 1) {
-                            System.out.println("No se encontro.");
+                        for (int i = 0; i < data.getSize(); i++) {
+                            if (data.getData().get(i).getCity().toLowerCase().equals(name.toLowerCase())) {
+                                System.out.println("Pais No." + (i + 1) + data.mostrarPais(i));
+                                break;
+                            } else if (i == data.getSize() - 1) {
+                                System.out.println("No se encontro.");
+                            }
                         }
-                    }
+                    } else
+                        System.out.println("No existen datos almacenados.");
                     break;
                 // Si ingresa un digito incorrecto del menu
                 default:
