cf33a37ae2312ce8ce2bc9c702700b2099acd692
Omar Flores Salazar <omar.flores.salazar@uabc.edu.mx>
2019-09-06 18:56:48 -0700

    Se ha añadido la practica dos de laboratorio

--- Algoritmos y Estructura de Datos/Laboratorio/Practica_2/Controller.java
+++ Algoritmos y Estructura de Datos/Laboratorio/Practica_2/Controller.java
@@ -14,26 +14,16 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.Stack;
 
+/**
+ * Clase que hace funcion como el controlador del programa usando el metodo MVC
+ *
+ * @author Omar
+ */
 public class Controller implements Initializable {
 
-    static Stack<Button> stack = new Stack<Button>();
+    private static Stack<BackgroundFill> stack = new Stack<BackgroundFill>();
+    private int pointer = 10;
 
-    @FXML
-    private Button button4;
-    @FXML
-    private Button button5;
-    @FXML
-    private Button button2;
-    @FXML
-    private Button button3;
-    @FXML
-    private Button button8;
-    @FXML
-    private Button button9;
-    @FXML
-    private Button button6;
-    @FXML
-    private Button button7;
     @FXML
     private Label labelMessage;
     @FXML
@@ -43,27 +33,104 @@ public class Controller implements Initializable {
     @FXML
     private ColorPicker colorPicker;
     @FXML
-    private Button button0;
-    @FXML
-    private Button button1;
-    @FXML
     private Button buttonRemoveTop;
 
+    /**
+     * Funcion que remueve el ultimo color agregado de la pila
+     *
+     * @param event Recibe la accion de un boton si se produce
+     */
     @FXML
-    void removeColorOnStack(ActionEvent event) {
-        System.out.println(((Button) event.getSource()).getId());
+    public void removeTop(ActionEvent event) {
+        buttonRemoveTop.setDisable(false);
+        if (!stack.isEmpty()) {
+            gridPaneStack.getChildren().get(pointer).setVisible(false);
+            gridPaneStack.getChildren().get(pointer).setDisable(true);
+
+            pointer++;
+            labelMessage.setText("Se ha removido un color: " + stack.peek().getFill());
+            stack.pop();
+        } else {
+            labelMessage.setText("La pila esta vacia.");
+            buttonRemoveTop.setDisable(true);
+        }
     }
 
+    /**
+     * Funcion que recibe la accion de un boton para poder eliminar de la pila
+     * el color seleccionado
+     *
+     * @param event Recibe la accion de un boton
+     */
     @FXML
-    void addColorToStack(ActionEvent event) {
-        BackgroundFill fill = new BackgroundFill(colorPicker.getValue(), null, null);
-        button0.setBackground(new Background(fill));
-        stack.push(button0);
-        //buttonRemoveTop.setDisable();
+    public void removeColorOnStack(ActionEvent event) {
+        int position = 0;
+        for (int i = 0; i < 10; i++) {
+            if (((Button) event.getSource()).getId().equals("button" + i)) {
+                position = i;
+                break;
+            }
+        }
+        Stack<BackgroundFill> temporalStack = new Stack<>();
+
+        int size = stack.size();
+        labelMessage.setText("Se ha removido un color: "
+                + ((Button) event.getSource()).getBackground().getFills().get(0).getFill()
+                + "en la posicion: " + position);
+
+        for (int i = position; i < size; i++) {
+            gridPaneStack.getChildren().get(9 - i).setVisible(false);
+            gridPaneStack.getChildren().get(9 - i).setDisable(true);
+        }
+        for (int i = position; i < size; i++) {
+            temporalStack.push(stack.pop());
+        }
+        temporalStack.pop();
+
+        pointer++;
+        for (int i = position; i < size - 1; i++) {
+            gridPaneStack.getChildren().get(9 - i).setVisible(true);
+            gridPaneStack.getChildren().get(9 - i).setDisable(false);
+            stack.push(temporalStack.pop());
+            ((Button) gridPaneStack.getChildren().get(9 - i)).setBackground(new Background(stack.peek()));
+
+        }
     }
 
+    /**
+     * Funcion que agrega un color a la pila
+     *
+     * @param event Recibe la accion si se produce una en el programa
+     */
+    @FXML
+    public void addColorToStack(ActionEvent event) {
+        if (stack.size() < 10) {
+            pointer--;
+            labelMessage.setText("Se agrego un nuevo color: " + colorPicker.getValue());
+
+            stack.push(new BackgroundFill(colorPicker.getValue(), null, null));
+            gridPaneStack.getChildren().get(pointer).setVisible(true);
+            ((Button) gridPaneStack.getChildren().get(pointer)).setBackground(new Background(stack.peek()));
+            gridPaneStack.getChildren().get(pointer).setDisable(false);
+        } else {
+            labelMessage.setText("La pila esta llena.");
+        }
+        buttonRemoveTop.setDisable(false);
+    }
+
+    /**
+     * Inicializa los requisitos iniciales del programa
+     *
+     * @param location
+     * @param resources
+     */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         buttonRemoveTop.setDisable(true);
+
+        for (int i = 0; i < 10; i++) {
+            gridPaneStack.getChildren().get(i).setDisable(true);
+            gridPaneStack.getChildren().get(i).setVisible(false);
+        }
     }
 }
--- Algoritmos y Estructura de Datos/Laboratorio/Practica_2/Main.java
+++ Algoritmos y Estructura de Datos/Laboratorio/Practica_2/Main.java
@@ -6,15 +6,31 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * Lee el archivo FXML para poder mostrar la interfaz del programa
+ *
+ * @author Omar
+ */
 public class Main extends Application {
 
+    /**
+     * Ejecuta JavaFX
+     *
+     * @param args Recibe los argumentos desde consola
+     */
     public static void main(String[] args) {
         launch(args);
     }
 
+    /**
+     * Parametriza la escena del programa
+     *
+     * @param primaryStage Recibe argumentos del escenario
+     * @throws Exception Excepcion para archivo inexistente en el programa
+     */
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("view.fxml"));
         primaryStage.setTitle("Pila de Colores");
         primaryStage.setScene(new Scene(root, 600, 600));
         primaryStage.show();
