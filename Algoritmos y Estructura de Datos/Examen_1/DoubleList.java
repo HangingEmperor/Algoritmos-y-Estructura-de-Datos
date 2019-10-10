45a67470c6a708804c8a72bf2fc9090f5de51b8e
Omar Flores Salazar <omar.flores.salazar@uabc.edu.mx>
2019-10-09 19:32:55 -0700

    Avances en examen 1 y practica 5

--- Algoritmos y Estructura de Datos/Examen_1/DoubleList.java
+++ Algoritmos y Estructura de Datos/Examen_1/DoubleList.java
@@ -3,6 +3,7 @@ package Examen_1;
 public class DoubleList<T> {
 
     private Node<T> start;
+    private int size = 0;
 
     public void insertStart(T data) {
         Node<T> node = new Node<>();
@@ -14,6 +15,7 @@ public class DoubleList<T> {
             start.setAnt(node);
         }
         start = node;
+        size++;
     }
 
     public void insertEnd(T data) {
@@ -32,6 +34,7 @@ public class DoubleList<T> {
             r.setSig(node);
             node.setAnt(r);
         }
+        size++;
     }
 
     public Node<T> removeStart() {
@@ -45,6 +48,7 @@ public class DoubleList<T> {
                 start.setAnt(null);
             }
         }
+        size--;
         return start;
     }
 
@@ -63,7 +67,7 @@ public class DoubleList<T> {
                 return r;
             }
         }
-
+        size--;
         return r;
     }
 
@@ -75,11 +79,70 @@ public class DoubleList<T> {
         } else {
             r = start;
             while (r != null) {
-                aux += r.getInfo();
+                aux += r.getInfo() + "\n";
                 r = r.getSig();
             }
         }
         return aux;
     }
 
+    public Node<T> remove(int position) {
+        if (position <= size) {
+            int informacion;
+            if (position == 1) {
+                informacion = (int) start.getInfo();
+                start = start.getSig();
+                if (start != null)
+                    start.setAnt(null);
+            } else {
+                Node<T> reco;
+                reco = start;
+                for (int f = 1; f <= position - 2; f++)
+                    reco = reco.getSig();
+                Node<T> prox = reco.getSig();
+                reco.setSig(prox.getSig());
+                Node<T> siguiente = prox.getSig();
+                if (siguiente != null)
+                    siguiente.setAnt(reco);
+                informacion = (int) prox.getInfo();
+            }
+            return start;
+        } else
+            return null;
+    }
+
+    public void insert(T data, int position) {
+        if (start == null) {
+            Node<T> node = new Node<>();
+            node.setInfo(data);
+            if (position == 1) {
+                node.setSig(start);
+                if (start != null) {
+                    start.setAnt(node);
+                }
+                start = node;
+            } else {
+                if (start.getSig() == null) {
+                    Node<T> r = new Node<>();
+                    r = start;
+                    while (r.getSig() != null) {
+                        r = r.getSig();
+                    }
+                    r.setSig(node);
+                    node.setAnt(r);
+                    node.setSig(null);
+                } else {
+                    Node<T> r = new Node<>();
+                    for (int f = 1; f <= position; f++) {
+                        r = r.getSig();
+                    }
+                    Node<T> s = r.getSig();
+                    r.setSig(node);
+                    node.setAnt(r);
+                    node.setSig(s);
+                    s.setAnt(node);
+                }
+            }
+        }
+    }
 }
