1e6cd2663a203266a55ae47b973eda212ba9c329
Omar Flores Salazar <31521501+HangingEmperor@users.noreply.github.com>
2019-10-22 19:17:02 -0500

    Update Controller.java

--- Algoritmos y Estructura de Datos/Laboratorio/Practica_4/Controller.java
+++ Algoritmos y Estructura de Datos/Laboratorio/Practica_4/Controller.java
@@ -24,8 +24,9 @@ public class Controller implements Initializable {
     private int size = 0;
 
     private int generarTarea() {
-        if (((int) (Math.random() * 10) + 1) % 2 == 0)
+        if (((int) (Math.random() * 10) + 1) % 2 == 0) {
             return (int) (Math.random() * 10) + 2;
+        }
         return 0;
     }
 
@@ -33,13 +34,8 @@ public class Controller implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         do {
             time++;
-            if (time % 3 == 0) {
-                int proceso = generarTarea();
-                if (proceso < 2) {
-                    proceso = 2;
-                } else if (proceso > 10) {
-                    proceso = 10;
-                }
+            int proceso = generarTarea();
+            if (proceso != 0) {
                 procesos++;
                 size++;
 
@@ -47,47 +43,56 @@ public class Controller implements Initializable {
                 textAreaProcesosGenerados.setText(textAreaProcesosGenerados.getText() + "Proceso " + process.getId()
                         + " se genero en t: " + process.getStart() + " seg\n" + "Tiempo ejecucion: "
                         + process.getTime() + " seg\n\n");
+                textAreaProcesosGenerados.setText(textAreaProcesosGenerados.getText() + " - - - - - - - - - -"
+                        + " - - - - - - - - - - - - - - - - - - - \n");
 
                 queu.insert(process);
-
-                for (int i = 0; i < size; i++) {
+            }
+   
+            if (time % 3 == 0 && size > 0) {
+                System.out.println("time: " + time + " size: " + size);
+                for (int i = 0; i <= size; i++) {
                     if (queu.front().getInfo().getTime() > 3) {
                         queu.front().getInfo().setTime(queu.front().getInfo().getTime() - 3);
-                        textAreaProcesosGenerados.setText(textAreaProcesosGenerados.getText() + "Proceso " +
-                                queu.front().getInfo().getId() + " reinsertado en tiempo: " + time + "seg\n");
+                        textAreaProcesosGenerados.setText(textAreaProcesosGenerados.getText() + "Proceso "
+                                + queu.front().getInfo().getId() + " reinsertado en tiempo: " + time + " seg\n");
                         queu.insert(queu.remove().getInfo());
                     } else if (queu.front().getInfo().getTime() < 3) {
                         time = time + queu.front().getInfo().getTime();
                         queu.front().getInfo().setEnd(time);
-                        textAreaProcesosTerminados.setText(textAreaProcesosTerminados.getText() + "Proceso: " +
-                                queu.front().getInfo().getId() + "\nTiempo inicio: " + queu.front().getInfo().getStart() +
-                                "\nTiempo ejecucion: " + queu.front().getInfo().getRequerido() + "\nTiempo restante: " + 0 +
-                                "\nTiempo final: " + queu.front().getInfo().getEnd() + "\nTiempo requerido: " +
-                                (time - queu.front().getInfo().getStart()) + "\n\n");
-                        textAreaProcesosGenerados.setText(textAreaProcesosGenerados.getText() + "Proceso: " +
-                                queu.front().getInfo().getId() + " se inserto en " + "Procesos Terminados\n");
+                        textAreaProcesosTerminados.setText(textAreaProcesosTerminados.getText() + "Proceso: "
+                                + queu.front().getInfo().getId() + "\nTiempo inicio: " + queu.front().getInfo().getStart()
+                                + "\nTiempo ejecucion: " + queu.front().getInfo().getRequerido() + "\nTiempo restante: " + 0
+                                + "\nTiempo final: " + queu.front().getInfo().getEnd() + "\nTiempo requerido: "
+                                + (time - queu.front().getInfo().getStart()) + "\n\n");
+                        textAreaProcesosGenerados.setText(textAreaProcesosGenerados.getText() + "Proceso: "
+                                + queu.front().getInfo().getId() + " se inserto en " + "Procesos Terminados\n");
                         queueEnd.insert(queu.remove().getInfo());
                         size--;
                     } else {
                         queu.front().getInfo().setEnd(time);
-                        textAreaProcesosTerminados.setText(textAreaProcesosTerminados.getText() + "Proceso: " +
-                                queu.front().getInfo().getId() + "\nTiempo inicio: " + queu.front().getInfo().getStart() +
-                                "\nTiempo ejecucion: " + queu.front().getInfo().getRequerido() + "\nTiempo restante: " +
-                                queu.front().getInfo().getTime() + "\nTiempo final: " + queu.front().getInfo().getEnd() +
-                                "\nTiempo requerido: " + (time - queu.front().getInfo().getStart()) + "\n\n");
+                        textAreaProcesosTerminados.setText(textAreaProcesosTerminados.getText() + "Proceso: "
+                                + queu.front().getInfo().getId() + "\nTiempo inicio: " + queu.front().getInfo().getStart()
+                                + "\nTiempo ejecucion: " + queu.front().getInfo().getRequerido() + "\nTiempo restante: "
+                                + queu.front().getInfo().getTime() + "\nTiempo final: " + queu.front().getInfo().getEnd()
+                                + "\nTiempo requerido: " + (time - queu.front().getInfo().getStart()) + "\n\n");
+                        textAreaProcesosGenerados.setText(textAreaProcesosGenerados.getText() + "Proceso: "
+                                + queu.front().getInfo().getId() + " se inserto en " + "Procesos Terminados\n");
                         queueEnd.insert(queu.remove().getInfo());
                         size--;
                     }
                 }
+                textAreaProcesosGenerados.setText(textAreaProcesosGenerados.getText() + " - - - - - - - - - -"
+                        + " - - - - - - - - - - - - - - - - - - - \n");
             }
-        } while (time < 100);
+        } while (time < 600);
 
         int size = queu.size();
         for (int i = 0; i < size; i++) {
-            textAreaProcesosNoTerminados.setText(textAreaProcesosNoTerminados.getText() + "Proceso: " +
-                    queu.front().getInfo().getId() + "\nTiempo inicio: " + queu.front().getInfo().getStart() +
-                    "\nTiempo restante: " + queu.front().getInfo().getTime() + "\nTiempo final: " + 0 +
-                    "\nTiempo requerido: " + 0 + "\n\n");
+            textAreaProcesosNoTerminados.setText(textAreaProcesosNoTerminados.getText() + "Proceso: "
+                    + queu.front().getInfo().getId() + "\nTiempo inicio: " + queu.front().getInfo().getStart()
+                    + "\nTiempo restante: " + queu.front().getInfo().getTime() + "\nTiempo final: " + 0
+                    + "\nTiempo requerido: " + 0 + "\n\n");
             queu.remove();
         }
         this.textAreaProcesosNoTerminados.setEditable(false);
