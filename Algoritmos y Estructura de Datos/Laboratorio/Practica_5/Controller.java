1fe561c8df40eada85680641a89816b9fd627eaa
Omar Flores Salazar <31521501+hangingemperor@users.noreply.github.com>
2019-10-28 13:36:42 -0600

    Avances importantes a la practica 5.

--- Algoritmos y Estructura de Datos/Laboratorio/Practica_5/Controller.java
+++ Algoritmos y Estructura de Datos/Laboratorio/Practica_5/Controller.java
@@ -1,4 +1,79 @@
 package Laboratorio.Practica_5;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.HBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private HBox list;
+    @FXML
+    private HBox listCopy;
+    @FXML
+    private Slider position;
+    @FXML
+    private ColorPicker colorPicker;
+
+    @FXML
+    void insert(ActionEvent event) {
+        if (!listCopy.getChildren().isEmpty()) {
+            ((Button) listCopy.getChildren().get(0)).setBackground(new Background(
+                    new BackgroundFill(colorPicker.getValue(), null, null)));
+            list.getChildren().add((int) position.getValue(), listCopy.getChildren().get(0));
+        }
+    }
+
+    @FXML
+    void insertStart(ActionEvent event) {
+        if (!listCopy.getChildren().isEmpty()) {
+            ((Button) listCopy.getChildren().get(0)).setBackground(new Background(
+                    new BackgroundFill(colorPicker.getValue(), null, null)));
+            list.getChildren().add(0, listCopy.getChildren().get(0));
+        }
+    }
+
+    @FXML
+    void insertEnd(ActionEvent event) {
+        if (!listCopy.getChildren().isEmpty()) {
+            ((Button) listCopy.getChildren().get(0)).setBackground(new Background(
+                    new BackgroundFill(colorPicker.getValue(), null, null)));
+            list.getChildren().add(listCopy.getChildren().get(0));
+        }
+    }
+
+    @FXML
+    void remove(ActionEvent event) {
+        if (!list.getChildren().isEmpty()) {
+            listCopy.getChildren().add(list.getChildren().get((int) position.getValue()));
+        }
+    }
+
+    @FXML
+    void removeFirst(ActionEvent event) {
+        if (!list.getChildren().isEmpty()) {
+            listCopy.getChildren().add(list.getChildren().get(0));
+        }
+    }
+
+    @FXML
+    void removeEnd(ActionEvent event) {
+        if (!list.getChildren().isEmpty()) {
+            listCopy.getChildren().add(list.getChildren().get(list.getChildren().size() - 1));
+        }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
--- Algoritmos y Estructura de Datos/Laboratorio/Practica_5/Main.java
+++ Algoritmos y Estructura de Datos/Laboratorio/Practica_5/Main.java
@@ -1,9 +1,34 @@
 package Laboratorio.Practica_5;
 
-import java.util.LinkedList;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
+/**
+ * El programa consiste en realizar una torre de hanoi animando sus movimientos de forma visual para el usuario.
+ *
+ * @author Omar Flores Salazar
+ */
+public class Main extends Application {
+
+    /**
+     * @param args Argumentos para consola
+     */
     public static void main(String[] args) {
-        LinkedList linkedList;
+        launch(args);
+    }
+
+    /**
+     * @param primaryStage Escenario visual que se crea.
+     * @throws Exception En caso de no existir el archivo.
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("view.fxml"));
+        primaryStage.setTitle("Listas simplemente ligadas");
+        primaryStage.setScene(new Scene(root, 610, 305));
+        primaryStage.show();
     }
 }
